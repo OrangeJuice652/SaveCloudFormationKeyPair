9a45d14d6597a25ef14aa976b757c928f2a77055
Azamino28ki <miyamizu328@outlook.jp>
2024-03-16 23:20:58 +0900

    スクリプト作成

---
+++ save_cloudformation_keypair.sh
@@ -0,0 +1,6 @@
+# $1: キーペアのID
+# $2: キーペア名
+
+# name引数のスペースについて: https://stackoverflow.com/questions/53597272/aws-cli-ssm-get-parameters-invalidparameters-on-windows
+PRIVATE_KEY=$(aws ssm get-parameter --with-decryption --query "Parameter.Value" --output text --name " /ec2/keypair/$1")
+echo $PRIVATE_KEY > ~/.ssh/${2};
