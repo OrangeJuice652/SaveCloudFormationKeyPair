a50d94cb1d7ef0f8933f3b3060e783cb2977406f
OrangeJuice652 <49087471+OrangeJuice652@users.noreply.github.com>
2024-03-17 13:08:20 +0900

    Update save_cloudformation_keypair.sh
    保存するプライベートキーに拡張子追加

--- save_cloudformation_keypair.sh
+++ save_cloudformation_keypair.sh
@@ -3,4 +3,4 @@
 
 # name引数のスペースについて: https://stackoverflow.com/questions/53597272/aws-cli-ssm-get-parameters-invalidparameters-on-windows
 PRIVATE_KEY=$(aws ssm get-parameter --with-decryption --query "Parameter.Value" --output text --name " /ec2/keypair/$1")
-echo $PRIVATE_KEY > ~/.ssh/${2};
+echo $PRIVATE_KEY > ~/.ssh/${2}.pem;
