e1adec777ba22e75c7483e7ee37c3759fced7b39
Azamino28ki <miyamizu328@outlook.jp>
2024-04-07 09:19:34 +0900

    CloudFormationから直接キーペアIDを取得できるように修正

--- README.md
+++ README.md
@@ -4,11 +4,19 @@ AWSCloudFormationで生成したkeypairについて、privateキーをローカ�
 ## 概要
 - AWS CLIでパラメータストア内に記載されたプライベートキーを取得し、~/.ssh内にプライベートキーファイルを作成するスクリプト。
 - 前提
+  - CloudFormationのスタック作成時に、キーペアIDを出力している。
+- 基礎知識
   - AWS CloudFormation上で、EC2のキーペアを作成した際、プライベートキーは、パラメータストアに保存される。
     - パラメータストアが作成されるパラメータストア名: /ec2/keypair/<キーペアのID>
+    - パラメータストアに記載されたIDを取得し、~/.sshにプライベートキーファイルを作成する。
+
+## 引数
+1.  $1: キーペアIDを取得したいCloudFormationスタック名
+2.  $2: CloudFormationスタック作成時出力されたキーペアIDの出力キー名
+3.  $3: ~/.sshに保存する保存キーペア名
 
 ## コマンド
 
 ```
-save_cloudformation_keypair.sh <キーペアのID> <キーペア名>
+save_cloudformation_keypair.sh <取得したいCloudFormationスタック名> <キーペア名> <~/.sshに保存するキーペアファイル名(.pemを除く)>
 ```
--- save_cloudformation_keypair.sh
+++ save_cloudformation_keypair.sh
@@ -1,5 +1,9 @@
-# $1: キーペアのID
-# $2: キーペア名
+# $1: キーペアIDを取得したいCloudFormationスタック名
+# $2: CloudFormationスタック作成時出力されたキーペアIDの出力キー名
+# $3: ~/.sshに保存する保存キーペア名
+
+# キーペアIDを取得
+KeyPairID=$(aws cloudformation describe-stacks --stack-name $1 --query "Stacks[0].Outputs[?OutputKey=='$2'].OutputValue" --output text)
 
 # name引数のスペースについて: https://stackoverflow.com/questions/53597272/aws-cli-ssm-get-parameters-invalidparameters-on-windows
-aws ssm get-parameter --with-decryption --query "Parameter.Value" --output text --name " /ec2/keypair/$1" > ~/.ssh/${2}.pem
+aws ssm get-parameter --with-decryption --query "Parameter.Value" --output text --name " /ec2/keypair/$KeyPairID" > ~/.ssh/$3.pem
